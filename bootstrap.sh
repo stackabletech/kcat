82a57462a189c5641d368674a00f0effbe25f37f
Natalie Klestrup Röijezon <nat.roijezon@stackable.tech>
2024-07-09 14:28:18 +0200

    Bootstrap: Download libjansson over HTTPS

--- bootstrap.sh
+++ bootstrap.sh
@@ -141,7 +141,7 @@ build librdkafka "([ -f config.h ] || ./configure --prefix=$DEST $lrk_install_de
 github_download "edenhill/yajl" "edenhill" "libyajl"
 build libyajl "([ -d build ] || ./configure --prefix $DEST) && make install" || (echo "Failed to build libyajl: JSON support will probably be disabled" ; true)
 
-download http://www.digip.org/jansson/releases/jansson-2.12.tar.gz libjansson
+download https://digip.org/jansson/releases/jansson-2.12.tar.gz libjansson
 build libjansson "([[ -f config.status ]] || ./configure --enable-static --prefix=$DEST) && make && make install" || (echo "Failed to build libjansson: AVRO support will probably be disabled" ; true)
 
 github_download "apache/avro" "release-1.8.2" "avroc"
